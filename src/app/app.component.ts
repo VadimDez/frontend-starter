ef65f295ad09bf543917c3c86bd54132bff00ad5
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-09-17 22:06:50 +0200

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,33 @@ import { MainService } from './services/main.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  isLoadingPublic = false;
+  isLoadingPrivate = false;
+  publicResponse: any;
 
   constructor(private mainService: MainService) {
     this.mainService.getMe().subscribe((res: { accessToken: string }) => {
       localStorage.setItem('token', res.accessToken);
     });
+  }
 
-    mainService.getPublic().subscribe((res: { status: string }) => {
-      console.log('public', res);
+  getPublic() {
+    this.isLoadingPublic = true;
+    this.mainService.getPublic().subscribe((res: { status: string }) => {
+      this.publicResponse = res;
+      this.isLoadingPublic = false;
+    }, () => {
+      this.isLoadingPublic = false;
     });
   }
 
   getProtected() {
+    this.isLoadingPrivate = true;
     this.mainService.getProtected().subscribe((res: { status: string }) => {
       console.log('protected', res);
+      this.isLoadingPrivate = false;
     }, (err: any) => {
+      this.isLoadingPrivate = false;
       if (err.status === 401) {
         MainService.goToLogin();
       }
