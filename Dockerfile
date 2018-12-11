9f45b6d0225b7873fd0a09012f53f564a4c32c76
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-12-11 18:53:38 +0100

    prettier cfg

--- Dockerfile
+++ Dockerfile
@@ -21,6 +21,3 @@ ENV PORT 3000
 EXPOSE 3000
 
 CMD ["npm", "start"]
-
-
-
--- src/app/services/http-interceptor.ts
+++ src/app/services/http-interceptor.ts
@@ -1,21 +1,21 @@
-import { Injectable } from "@angular/core";
-import { of } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest
-} from "@angular/common/http";
+} from '@angular/common/http';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let r = req.clone();
-    const authToken = localStorage.getItem("token");
+    const authToken = localStorage.getItem('token');
 
     if (authToken) {
       r = req.clone({
-        headers: req.headers.set("Authorization", `Bearer ${authToken}`)
+        headers: req.headers.set('Authorization', `Bearer ${authToken}`)
       });
     }
 
